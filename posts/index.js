d2693e2c13b08b474e1ec5087993d9c8b02a7072
Joseph Mutua <Mutuaj793@gmail.com>
2022-04-13 21:04:07 +0300

    Emitting comment creation events

--- posts/index.js
+++ posts/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { randomBytes } = require("crypto");
-const cors = require("cors")
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 const posts = {};
 
@@ -13,7 +14,7 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
 
@@ -21,6 +22,14 @@ app.post("/posts", (req, res) => {
     id,
     title,
   };
+
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  });
 
   res.status(201).send(posts[id]);
 });
