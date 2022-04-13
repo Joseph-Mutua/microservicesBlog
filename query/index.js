696c3ed0a46d618cec164af581203098622d3f83
Joseph Mutua <Mutuaj793@gmail.com>
2022-04-13 21:50:29 +0300

    Parsing incoming events

--- query/index.js
+++ query/index.js
@@ -1,20 +1,37 @@
-const express = require("express")
-const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors)
+app.use(cors);
+
+const posts = {};
 
 app.get("/posts", (req, res) => {
+  res.send(posts);
+});
 
-})
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
 
-app.post("/posts", (req, res) => {
+  if (type === "PostCreated") {
+    const { id, title } = data;
 
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { id, content, postId } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+  console.log(posts)
+  res.send({});
 });
 
 app.listen(4002, (req, res) => {
-    console.log("Listening on port 4002")
-})
+  console.log("Listening on port 4002");
+});
