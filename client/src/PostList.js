37a57ccfa37029e03f75c2022dc05e14251f22aa
Joseph Mutua <Mutuaj793@gmail.com>
2022-04-13 18:45:51 +0300

    Displaying comments

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
 
 const PostList = () => {
   const [posts, setPosts] = useState({});
@@ -17,9 +18,14 @@ const PostList = () => {
 
   const renderedPosts = Object.values(posts).map((post) => {
     return (
-      <div className="card" style={{ width: "30%", marginBottom: "20px" }}>
+      <div
+        key={post.id}
+        className="card"
+        style={{ width: "30%", marginBottom: "20px" }}
+      >
         <div className="card-body">
           <h3>{post.title}</h3>
+          <CommentList postId={post.id} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
